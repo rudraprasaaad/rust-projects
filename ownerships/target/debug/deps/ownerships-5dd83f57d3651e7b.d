9ba3609bc659c9e8a971705a34138e69eab9ae10
rudraprasaaad <goudarudra23@gmail.com>
2025-09-21 11:48:54 +0530

    done with ownership

--- ownerships/target/debug/deps/ownerships-5dd83f57d3651e7b.d
+++
@@ -1,5 +0,0 @@
-/home/rudra/Projects/rust-projects/ownerships/target/debug/deps/ownerships-5dd83f57d3651e7b.d: src/main.rs
-
-/home/rudra/Projects/rust-projects/ownerships/target/debug/deps/libownerships-5dd83f57d3651e7b.rmeta: src/main.rs
-
-src/main.rs:
